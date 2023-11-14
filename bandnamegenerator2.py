e8d5ed4e6a86c09ecc32a5f2862b90601169a67f
Bernadine-se <150375144+Bernadine-se@users.noreply.github.com>
2023-11-14 08:49:38 +0200

    Add files via upload

---
+++ bandnamegenerator2.py
@@ -0,0 +1,18 @@
+city = input ('what is the name of the city you grew up in?\n')
+pet =  input ('what is the name of your pet?\n')
+
+print ('Your band name could be:' ' '+ city + " " + pet)
+
+temp = city
+city = pet
+pet = temp
+
+print('Or it could be:' " "+ city + " " + pet)
+
+
+
+
+
+
+
+
